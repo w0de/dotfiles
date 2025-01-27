dae31dd58782ae597396766c3541ac92aaab7645
Harry Seeber <harry.seeber@faire.com>
2025-01-27 11:00:32 +0100

    sudo touchid and myip alias

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,5 +12,7 @@ echo "zsh installed"
 test -z "${BREWTSTRAP}" || ./brew.sh
 echo -e "\nInstalling rc files...\n"
 cp -r ./.{*{rc,config,_profile},vim,macos} $HOME/
+echo "sudo chown root:wheel sudo_local"
+chmod 444 sudo_local && sudo chown root:wheel sudo_local && sudo cp sudo_local /etc/pam.d/
 echo "source ~/.zsh_profile" >> $HOME/.zshrc
 exec zsh -l
