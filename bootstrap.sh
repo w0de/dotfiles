ecff271fd4ec971a84dde7c9f859e43398f93049
Harry Seeber <harry.seeber@faire.com>
2025-02-20 12:33:13 -0700

    add le guin quotes

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/
 echo "zsh installed"
 test -z "${BREWTSTRAP}" || ./brew.sh
 echo -e "\nInstalling rc files...\n"
-cp -r ./.{*{rc,config,_profile},vim,macos} $HOME/
+cp -r ./.{*{rc,config,_profile},vim,macos,json} $HOME/
 echo "sudo chown root:wheel sudo_local"
 chmod 444 sudo_local && sudo chown root:wheel sudo_local && sudo cp sudo_local /etc/pam.d/
 echo "source ~/.zsh_profile" >> $HOME/.zshrc
