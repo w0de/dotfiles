121bcf7abddeb8b8371f7ffd294075eeeebc8795
Harry Seeber <harry.seeber@faire.com>
2025-02-20 12:47:06 -0700

    improve quotes script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 set -eo pipefail
 
+u=$(whoami)
+
 if test -d $HOME/.oh-my-zsh; then
 	test -d $HOME/.oh-my-zsh.backups || mkdir $HOME/.oh-my-zsh.backups
 	mv $HOME/.oh-my-zsh "$HOME/.oh-my-zsh.backups/$(date +'%s')"
@@ -11,8 +13,11 @@ RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/
 echo "zsh installed"
 test -z "${BREWTSTRAP}" || ./brew.sh
 echo -e "\nInstalling rc files...\n"
-cp -r ./.{*{rc,config,_profile},vim,macos,json} $HOME/
+rm -f $HOME/.{shevek,hitchhiker,quotes}rc
+cp -r ./.{*{rc,config,_profile},vim,macos} $HOME/
+cp ./.quotes.json $HOME/
 echo "sudo chown root:wheel sudo_local"
 chmod 444 sudo_local && sudo chown root:wheel sudo_local && sudo cp sudo_local /etc/pam.d/
-echo "source ~/.zsh_profile" >> $HOME/.zshrc
+echo "source $HOME/.zsh_profile" >> $HOME/.zshrc
 exec zsh -l
+source $HOME/.zsh_profile
